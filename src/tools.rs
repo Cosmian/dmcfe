7fbe0ec611bce383aee1df21b76d6b25e37b664a
Théophile BRÉZOT <theophile.brezot@ens.fr>
2021-11-15 16:20:27 +0100

    Proofread and remove DLP-related code

--- src/tools.rs
+++ src/tools.rs
@@ -1,21 +1,12 @@
-#![allow(dead_code)]
-
 use bls12_381::{
     hash_to_curve::{ExpandMsgXmd, HashToCurve},
     G1Affine, G1Projective, G2Projective, Scalar,
 };
 use eyre::Result;
-use rand::Rng;
 use sha2::{Digest, Sha256, Sha512};
 use std::cmp::Ordering;
 
 const DST: &[u8] = b"simple_DST";
-
-/// Convert the given uint64 into a valid Fp scalar.
-/// - x:    the uint64
-pub(crate) fn integer_to_scalar(x: u64) -> Scalar {
-    Scalar::from_raw([x, 0, 0, 0])
-}
 
 /// Draw a random scalar from Fp.
 pub(crate) fn random_scalar() -> Scalar {
@@ -25,17 +16,6 @@ pub(crate) fn random_scalar() -> Scalar {
         rand::random(),
         rand::random(),
     ])
-}
-
-/// Draw a random scalar from Fp.
-pub(crate) fn bounded_random_scalar(min: u64, max: u64) -> Result<Scalar> {
-    eyre::ensure!(min < max, "min bound > max bound!");
-    Ok(Scalar::from_raw([
-        rand::thread_rng().gen_range(min..max),
-        0,
-        0,
-        0,
-    ]))
 }
 
 /// Hide a given scalar in G1 based on the CDH assumption.
@@ -48,22 +28,6 @@ pub(crate) fn smul_in_g1(a: &Scalar) -> G1Projective {
 /// - `a`:    scalar
 pub(crate) fn smul_in_g2(a: &Scalar) -> G2Projective {
     G2Projective::generator() * a
-}
-
-/// Generate a random point in G_1.
-pub(crate) fn random_in_g1() -> G1Projective {
-    smul_in_g1(&random_scalar())
-}
-
-/// Generate a random point in G_2.
-pub(crate) fn random_in_g2() -> G2Projective {
-    smul_in_g2(&random_scalar())
-}
-
-/// Returns the inverse of P in G1 in projective coordinates.
-/// - `P`:  Point in G1
-pub(crate) fn get_inverse(P: &G1Projective) -> G1Projective {
-    G1Projective::from(G1Affine::inverse(&G1Affine::from(P)))
 }
 
 /// Returns the hash of the given `usize` in `G1`
@@ -182,45 +146,6 @@ pub(crate) fn scal_mat_mul_dim_2(x: &[Vec<Scalar>], y: &[Scalar]) -> Result<Vec<
         .collect())
 }
 
-/// This algorithm implements the recursive version of the double-and-add method to compute `n.P`.
-/// - `P`:  point
-/// - `n`:  exponent
-pub(crate) fn double_and_add_rec(P: &G1Projective, n: u64) -> G1Projective {
-    let Q = *P;
-    if n == 0 {
-        G1Projective::identity()
-    } else if n == 1 {
-        Q
-    } else if (n % 2) == 1 {
-        Q + double_and_add_rec(&Q, n - 1)
-    } else {
-        double_and_add_rec(&Q.double(), n / 2)
-    }
-}
-
-/// This algorithm implements the double-and-add method to compute `n.P`.
-/// - `P`:  point
-/// - `n`:  exponent
-pub(crate) fn double_and_add(P: &G1Projective, n: u64) -> G1Projective {
-    let mut acc = G1Projective::identity();
-
-    // This is a simple double-and-add implementation of point
-    // multiplication, moving from most significant to least
-    // significant bit of the scalar.
-
-    for bit in n
-        .to_be_bytes() // big endian representation
-        .iter()
-        .flat_map(|byte| (0..8).rev().map(move |i| (byte >> i & 1u8)))
-    {
-        acc = acc.double();
-        if bit == 1u8 {
-            acc += P;
-        }
-    }
-    acc
-}
-
 /// Compute the `h_(i, j, l)` function of the DSum.
 /// - `l`:      label
 /// - `ski`:    some client secret key
@@ -234,35 +159,5 @@ pub(crate) fn h(label: &[u8], ski: &Scalar, pkj: &G1Projective) -> Scalar {
         Ordering::Less => Scalar::neg(&hash_to_scalar(pkj, &pki, &(pkj * ski), label)),
         Ordering::Equal => Scalar::zero(),
         Ordering::Greater => hash_to_scalar(&pki, pkj, &(pkj * ski), label),
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use crate::tools;
-    use bls12_381::G1Projective;
-    use eyre::Result;
-    use rand::Rng;
-
-    #[test]
-    fn test_double_and_add() -> Result<()> {
-        /// Compute the naive exponentiation `n.P`.
-        /// - `P`:  point
-        /// - `n`:  exponent
-        fn naive_exponentiation(P: &G1Projective, n: u64) -> G1Projective {
-            let mut Q = *P;
-            for _i in 1..n {
-                Q += P;
-            }
-            Q
-        }
-
-        let P = tools::random_in_g1();
-        let n: u64 = rand::thread_rng().gen_range(10..20);
-        eyre::ensure!(
-            naive_exponentiation(&P, n) == tools::double_and_add(&P, n),
-            "Error while computing the exponentiation: incorrect result!"
-        );
-        Ok(())
     }
 }
